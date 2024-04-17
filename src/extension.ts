3c6a08661d1437516be7aa777bf4f97dd0f6debf
Darrell Cole Hill <darrellcolehill@gmail.com>
2024-04-17 10:17:19 -0400

    1713363439578

--- src/extension.ts
+++ src/extension.ts
@@ -81,6 +81,11 @@ export function activate(context: vscode.ExtensionContext) {
 						parseXml(xmlFileContent, chapter, verse)
 						.then((result) => {
 							console.log(result);
+                            
+                            // TODO: iterate over results and get data from en_gwt
+
+                            
+
 						})
 						.catch((err) => {
 							console.error(err);
@@ -180,3 +185,76 @@ const xmlData = `
     </book>
 </xml>
 `;
+
+
+
+// Takes the target strongs number and calculates its parent folder in the en_gwt repo
+// This is greatly dependent on the current structure of the en_gwt
+function getStrongsRange(strongs : String) {
+	let thousandsDigit = strongs.charAt(1);
+	let hundredsDigit = strongs.charAt(2);
+	let tensDigit = strongs.charAt(3);
+	let onesDigit = strongs.charAt(4);
+
+	let strongsNumberStr : string =
+		thousandsDigit + hundredsDigit + tensDigit + onesDigit;
+
+	let strongsNumber = parseInt(strongsNumberStr);
+
+	let startStrongsRangeNumber;
+	let endStrongsRangeNumber;
+
+	if (strongsNumber <= 10) {
+		return "g0001-g0010";
+	} else if (parseInt(onesDigit) === 0) {
+		startStrongsRangeNumber = strongsNumber - 9;
+		endStrongsRangeNumber = strongsNumber;
+	} else {
+		startStrongsRangeNumber =
+			strongsNumber - (strongsNumber % 10) + 1;
+		endStrongsRangeNumber =
+			strongsNumber - (strongsNumber % 10) + 10;
+	}
+
+	let startStrongsRangeString = makeFourDigitStrongs(
+		"g" + startStrongsRangeNumber
+	);
+	let endStrongsRangeString = makeFourDigitStrongs(
+		"g" + endStrongsRangeNumber
+	);
+
+	let strongsRange = (startStrongsRangeString + "-").concat(
+		endStrongsRangeString
+	);
+
+	return strongsRange.toLocaleLowerCase();
+}
+
+
+// Adds padding zeros (to the second character's posision) until it's length is 5
+function makeFourDigitStrongs(strongs : string) {
+	if (strongs.length < 5 && strongs.length >= 2) {
+		for (let i = strongs.length; i < 5; i++) {
+			strongs = stringInsert(strongs, 1, "0", false);
+		}
+	}
+
+	return strongs;
+}
+
+
+// utility function to insert a string into another string
+function stringInsert(str: string, index : number, value : string, replace : boolean) {
+	let res = "";
+
+	if (replace === false) {
+		res = str.substr(0, index) + value + str.substr(index);
+	} else {
+		res =
+			str.substr(0, index) +
+			value +
+			str.substr(index + value.length);
+	}
+
+	return res;
+}
